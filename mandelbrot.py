b2372f164afdddaec57c10e1c7182f15857552b8
zacharyducore <zacharyducore@ufl.edu>
2025-03-13 21:51:03 -0400

    Added Julia Set and docstring

--- mandelbrot.py
+++ mandelbrot.py
@@ -13,18 +13,6 @@ def get_escape_time(c: complex, max_iterations: int) -> int | None:
         z = z**2 + c
     return None
 
-#Part 2
-def get_complex_grid(
-        top_left: complex,
-        bottom_right: complex,
-        step: float,
-) -> np.ndarray:
-    real_value = np.arange(top_left.real, bottom_right.real, step) #Generate Real Values
-    imag_value = np.arange(top_left.imag, bottom_right.imag, -step) #Generate Imaginary Values
-
-    grid = real_value[np.newaxis, :] + 1j * imag_value[:, np.newaxis] #Generate Complex Grid
-
-    return grid
 
 
 def get_julia_escape_time(z: complex, c: complex, max_iterations: int) -> int:
